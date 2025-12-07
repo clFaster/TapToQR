f907030ac23cd4febd1827ad3f4477bb9351b73e
Moritz Reis <me@moritzreis.dev>
2025-12-07 16:24:16 +0100

    fix: adjust width of download buttons container in Home component

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
               </p>
             </div>
             {/* Download Buttons */}
-            <div className="w-full max-w-md">
+            <div className="w-full">
               <BrowserStoreLink showAll={true} />
             </div>
           </div>
